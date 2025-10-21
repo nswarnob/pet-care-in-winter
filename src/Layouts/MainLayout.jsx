e11ebda71b760f8b60adca828688fe8e5d0544dc
Sharif Uddin Arnob <nswarnob@gmail.com>
2025-10-22 03:20:11 +0600

    project set up

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div>
+           <header>
+            <Navbar></Navbar>
+           </header>
+            <main>
+                <Outlet></Outlet>
+            </main>
+        <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+
+export const router = createBrowserRouter(
+
+    [
+     {
+        path: "/",
+        element: <MainLayout></MainLayout>
+     },
+        
+    ]
+)
